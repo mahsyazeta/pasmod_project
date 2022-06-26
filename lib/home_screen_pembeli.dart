55f1a91d222c6c28179f92e7d1c2a572b77da819
mahsyazeta <zetadhana@gmail.com>
2022-06-26 23:49:23 +0700

    error code

--- lib/home_screen_pembeli.dart
+++ lib/home_screen_pembeli.dart
@@ -3,7 +3,15 @@ import 'package:flutter/material.dart';
 class HomeScreenPembeli extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('gass');
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            
+          ),
+        ],
+      ),
+    );
   }
   
 }
